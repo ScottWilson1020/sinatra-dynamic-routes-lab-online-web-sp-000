2f79db9e02b4fe541a4e751c2490753cd622b54e
Scott Wilson <scottwilson1020@gmail.com>
2020-12-13 23:43:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,4 +3,10 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get "/hello/:name" do
+    @user_name = params[:name]
+    "Hello #{@user_name}!"
+  end
+
+
 end
